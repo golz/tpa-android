f9678d171f40726df18290fccbf14bebf660d017
mneysa <michelleneysa@gmail.com>
2016-12-04 14:54:13 +0700

    Facebook Auth

--- app/src/main/java/com/michelle/goldwin/tpamobile/activity/LoginActivity.java
+++ app/src/main/java/com/michelle/goldwin/tpamobile/activity/LoginActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telecom.Call;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -22,8 +22,12 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.michelle.goldwin.tpamobile.R;
 
 import java.util.Arrays;
@@ -33,7 +37,6 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText txtEmail;
     private EditText txtPassword;
-    private Button btnFacebook;
     private Button btnSignin;
     private Button btnSignup;
 
@@ -55,7 +58,8 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                facebookToken(loginResult.getAccessToken());
+
             }
 
             @Override
@@ -73,7 +77,6 @@ public class LoginActivity extends AppCompatActivity {
         /* BEGIN INITIALIZE */
         txtEmail    = (EditText) findViewById(R.id.txtEmail);
         txtPassword = (EditText)findViewById(R.id.txtPassword);
-        btnFacebook = (Button) findViewById(R.id.btnFacebook);
         btnSignin   = (Button) findViewById(R.id.btnSignin);
         btnSignup   = (Button) findViewById(R.id.btnSignup);
 
@@ -139,6 +142,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode,resultCode,data);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+        startActivity(intent);
         Log.e("data",data.toString());
     }
+
+    public void facebookToken(AccessToken token){
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+            }
+        });
+
+    }
+
 }

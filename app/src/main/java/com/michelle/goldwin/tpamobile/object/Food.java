1375f27948a456747ae4749118becc601de25f58
Goldwin <golzwinsjapar@gmail.com>
2016-12-06 01:28:39 +0700

    Add Insert food layout, insert food to firebase but not yet retrieve food from firebase, see TODO

---
+++ app/src/main/java/com/michelle/goldwin/tpamobile/object/Food.java
@@ -0,0 +1,18 @@
+package com.michelle.goldwin.tpamobile.object;
+
+/**
+ * Created by Goldwin on 6/12/2016.
+ */
+
+public class Food {
+    public String foodname;
+    public Double calorie;
+
+    public Food() {
+    }
+
+    public Food(String foodname, Double calorie) {
+        this.foodname = foodname;
+        this.calorie = calorie;
+    }
+}
